ecbb54e422535c9f4b6f49ca3fe0c5b317ab7318
John Bugner <john.bugner@gmail.com>
2020-06-04 19:58:23 -0500

    Used Broswer.sandbox.

--- src/Main.elm
+++ src/Main.elm
@@ -1,12 +1,42 @@
 module Main exposing (..)
 
+import Browser as B
 import Html as H
 import Html.Attributes as HA
+import Html.Events as HE
 
 main =
-    H.div []
+    B.sandbox
+        { init = init
+        , update = update
+        , view = view
+        }
+
+type alias Model =
+    { input : String
+    }
+
+init : Model
+init =
+    { input = ""
+    }
+
+type Event
+    = Solve String
+
+update : Event -> Model -> Model
+update event model =
+    case event of
+        Solve newInput -> { model | input = newInput }
+
+view : Model -> H.Html Event
+view model =
+    H.div
+        []
         [ H.textarea
             [ HA.placeholder "input"
+            , HA.value model.input
+            , HE.onInput Solve
             ]
             []
         , H.input
@@ -17,6 +47,7 @@ main =
         , H.textarea
             [ HA.placeholder "output"
             , HA.readonly True
+            , HA.value <| String.reverse model.input
             ]
             []
         ]
